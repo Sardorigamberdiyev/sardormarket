fa9816d1e5d240e310ea40f16a328576bc844a51
Sardorigamberdiyev <72994594+Sardorigamberdiyev@users.noreply.github.com>
2020-10-20 01:56:57 +0500

    Updates

---
+++ precache-manifest.16e9431fe48af8b9cb47cf98f1d9e70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9cfd97230c03ec80da73e12f52125e4",
+    "url": "/sardormarket/index.html"
+  },
+  {
+    "revision": "1d9b82e2d07b03ff32d9",
+    "url": "/sardormarket/static/css/main.ceebb911.chunk.css"
+  },
+  {
+    "revision": "285501f6fb9bac117f6b",
+    "url": "/sardormarket/static/js/2.36754238.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sardormarket/static/js/2.36754238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9b82e2d07b03ff32d9",
+    "url": "/sardormarket/static/js/main.246bdcb7.chunk.js"
+  },
+  {
+    "revision": "dd8ea3c6d995f7152aff",
+    "url": "/sardormarket/static/js/runtime-main.c4a1b35f.js"
+  }
+]);
