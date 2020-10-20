7bb3f7058aec4fb09312b42d7de5f6087d1e5e79
Sardorigamberdiyev <72994594+Sardorigamberdiyev@users.noreply.github.com>
2020-10-20 15:42:39 +0500

    Updates

--- precache-manifest.5e6341962650110bffe48f6b0d188074.js
+++ precache-manifest.5e6341962650110bffe48f6b0d188074.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e9cfd97230c03ec80da73e12f52125e4",
+    "revision": "8f84453283a30693ce83cce26109a6c4",
     "url": "/sardormarket/index.html"
   },
   {
-    "revision": "1d9b82e2d07b03ff32d9",
+    "revision": "4e703aceb51d7b0cb4f0",
     "url": "/sardormarket/static/css/main.ceebb911.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sardormarket/static/js/2.36754238.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d9b82e2d07b03ff32d9",
-    "url": "/sardormarket/static/js/main.246bdcb7.chunk.js"
+    "revision": "4e703aceb51d7b0cb4f0",
+    "url": "/sardormarket/static/js/main.7c02abb9.chunk.js"
   },
   {
     "revision": "dd8ea3c6d995f7152aff",
